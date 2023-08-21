760d41bf52d9e5bb834a08d7141db66b7d46d069
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-08-20 23:14:33 -0700

    Enable GA4 in dev (breaks otherwise)

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,21 +3,13 @@
 </script>
 
 <svelte:head>
-  {#if process.env.NODE_ENV === "production"}
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-43NJXZGJ1M"
-    ></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
-      gtag("config", "G-43NJXZGJ1M");
-    </script>
-  {/if}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-43NJXZGJ1M" />
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag("js", new Date());
+    gtag("config", "G-43NJXZGJ1M");
+  </script>
 </svelte:head>
 
 <main>
